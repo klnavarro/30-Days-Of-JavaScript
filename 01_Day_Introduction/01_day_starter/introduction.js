4bcc701f38a33b497cbdbbc2af50e26e5211a9f0
Kathleen Navarro <kthlnnvrr@gmail.com>
2026-02-08 22:24:50 -0800

    day 1 stuff pt 1

--- 01_Day_Introduction/01_day_starter/introduction.js
+++ 01_Day_Introduction/01_day_starter/introduction.js
@@ -1,1 +1,8 @@
 console.log('Welcome to 30DaysOfJavaScript')
+
+console.log("2+3=",(2 + 3)) // Addition
+console.log("3-2=",(3 - 2)) // Subtraction
+console.log("2*3=",(2 * 3)) // Multiplication
+console.log("3/2=",(3 / 2)) // Division
+console.log("3%2=",(3 % 2)) // Modulus - finding remainder
+console.log("3**2=",(3 ** 2))
